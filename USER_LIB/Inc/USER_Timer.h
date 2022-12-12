ed771e3556dd95c004224fd6253f5aa16aea8939
JamesChiDuong <chiduong1312@gmail.com>
2022-12-12 21:31:00 +0800

    Add Function USER TIMER

---
+++ USER_LIB/Inc/USER_Timer.h
@@ -0,0 +1,48 @@
+#ifndef TIMER_FUNCTION_USER
+#define TIMER_FUNCTION_USER
+
+#include <USER_CALLBACK_FUNCTION.h>
+#include "stm32g4xx_hal.h"
+typedef struct COMPONENT_TIMER
+{
+	float Duty;
+	uint16_t NumberOfTimer;
+	uint32_t Channel;
+	uint32_t frequency;
+}COMPONENT_TIMER;
+/**
+  * @brief  The Function to choose the property of PWM such as, Dutycycle, The number and channel of Timer
+  * @note   Parameters of this function
+  *          - DutyCycle: To choose the expected dutycyle
+  *          - htim: The number of Timer
+  *          - TimerChannel: The number channel of timer
+  */
+void USER_TIMER_setValueOfPWM(COMPONENT_TIMER TimerVariable);/************* Funtion to set PWM*/
+
+/**
+  * @brief  The Function to handler String From USART to put into the timer variable
+  * @note   Parameters of this function
+  *          - Buffer: Which is stored Data
+  */
+void USER_TIMER_setFrequency(uint32_t ExpectedFrequency, uint16_t NumberOfTimer,uint16_t TimerChannel);
+
+COMPONENT_TIMER USER_TIMER_handleString(char* Buffer);
+/**
+  * @brief  To get value from ARR Register
+  * @note   Parameters of this function
+  *          - None
+  */
+uint32_t USER_TIMER_getValueARR(void);
+/**
+  * @brief  To get value from CRR Register
+  * @note   Parameters of this function
+  *          - None
+  */
+uint32_t USER_TIMER_getValueCCR(void);
+/**
+  * @brief  Convert ADC value to Dutycycle. Voltage 0 - 3.3V conver 0->100%
+  * @note   Parameters of this function
+  *          - ADC value
+  */
+float USER_TIMER_ConvertADCValueToDutyCycle(float ADCValue);
+#endif
---
+++ USER_LIB/Src/USER_Timer.c
@@ -0,0 +1,102 @@
+/*
+ * Timer_USER.c
+ *
+ *  Created on: Oct 18, 2022
+ *      Author: PC
+ */
+#include <USER_Timer.h>
+#include <assert.h>
+/*Declare Variable */
+uint32_t AHBFrequency = 150000000;
+uint32_t ValueOfARR = 0;
+uint32_t ValueOfCCR = 0;
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+/****Static Prototype *****/
+static uint16_t __USER_TIMER_convertU16(int DutyCycle);
+
+/****Define function********************/
+static uint16_t __USER_TIMER_convertU16(int DutyCycle)
+{
+	return (uint16_t)DutyCycle;
+}
+
+
+void USER_TIMER_setValueOfPWM(COMPONENT_TIMER TimerVariable)
+{
+	TIM_HandleTypeDef htim;
+	switch(TimerVariable.NumberOfTimer)
+	{
+		case 2:
+			htim = htim2;
+			break;
+		case 3:
+			htim = htim3;
+			break;
+	}
+	switch (TimerVariable.Channel)
+	{
+		case 1: /* Channel to trigger ADC */
+			TimerVariable.Channel = TIM_CHANNEL_1;
+			break;
+		case 2: /* Channel to PWM */
+			TimerVariable.Channel = TIM_CHANNEL_2;
+			break;
+		case 3:
+			TimerVariable.Channel = TIM_CHANNEL_3;
+			break;
+	}
+	//assert((TimerVariable.frequency >=100000) || TimerVariable.frequency == 0);
+	switch (TimerVariable.frequency)
+	{
+		case 0:
+			ValueOfARR = (uint32_t)__HAL_TIM_GET_AUTORELOAD(&htim);
+			break;
+		default:
+			htim.Instance->ARR = ((uint32_t)((AHBFrequency)/(TimerVariable.frequency*(htim.Init.Prescaler+1))) -1);
+			ValueOfARR = (uint32_t)__HAL_TIM_GET_AUTORELOAD(&htim);
+			break;
+	}
+	ValueOfCCR = (((uint32_t)TimerVariable.Duty*ValueOfARR)/100);
+	__HAL_TIM_SET_COMPARE(&htim,TimerVariable.Channel,ValueOfCCR);
+}
+/*Syntax buffer TIMER-30-20000-2-2(TIMER-dutycycle-tanso-numberoftimer-channel) */
+COMPONENT_TIMER USER_TIMER_handleString(char* Buffer)
+{
+	char* ptr = NULL;
+	COMPONENT_TIMER var = {0.0,0,0,0};
+
+
+	ptr = strtok(Buffer,"-");
+	ptr = strtok(NULL,"-");
+	var.Duty = __USER_TIMER_convertU16(atoi(ptr));
+	ptr = strtok(NULL,"-");
+	var.frequency = (uint32_t)atoi(ptr);
+	ptr = strtok(NULL,"-");
+	var.NumberOfTimer = __USER_TIMER_convertU16(atoi(ptr));
+	ptr = strtok(NULL,"-");
+	var.Channel = __USER_TIMER_convertU16(atoi(ptr));
+	return var;
+}
+uint32_t USER_TIMER_getValueARR(void)
+{
+	return ValueOfARR;
+}
+uint32_t USER_TIMER_getValueCCR(void)
+{
+	return ValueOfCCR;
+}
+
+/**
+  * @brief  Convert ADC value to Dutycycle. Voltage 0 - 3.3V conver 0->100%
+  * @note   Parameters of this function
+  *          - ADC value
+  */
+float USER_TIMER_ConvertADCValueToDutyCycle(float ADCValue)
+{
+	float tempValue = (ADCValue/1000);
+	float DutyCycle = 0;
+
+	DutyCycle = (tempValue*100)/3.3;
+	return DutyCycle;
+}
